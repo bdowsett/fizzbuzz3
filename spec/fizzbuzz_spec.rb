b76fc1341b75b9993c2891fda3b1217527fe2a00
Ben Dowsett <bendowsett@gmail.com>
2021-10-14 10:59:20 +0000

    third test for 5 & 3 passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,5 +8,9 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
    expect(fizzbuzz(5)).to eq 'buzz' 
   end
+  
+  it 'returns "fizzbuzz" when passed number divisible by 3 & 5' do 
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end 
 
 end
